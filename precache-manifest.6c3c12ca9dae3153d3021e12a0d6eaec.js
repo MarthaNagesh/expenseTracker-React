24d51ac60e09b2a2816cb64b39b4d2217eef0a15
nagesh <marthanagesh@gmail.com>
2020-03-29 13:43:57 -0400

    Updates

---
+++ precache-manifest.6c3c12ca9dae3153d3021e12a0d6eaec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdcbde6d1787c5419143e3f0cf55d88",
+    "url": "/expenseTracker-React/index.html"
+  },
+  {
+    "revision": "f4358fcc4f35cfba3bd6",
+    "url": "/expenseTracker-React/static/css/main.46a2462f.chunk.css"
+  },
+  {
+    "revision": "6c4a503a07e7c39835e6",
+    "url": "/expenseTracker-React/static/js/2.fe0fce3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expenseTracker-React/static/js/2.fe0fce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4358fcc4f35cfba3bd6",
+    "url": "/expenseTracker-React/static/js/main.4e45991c.chunk.js"
+  },
+  {
+    "revision": "f4ff048683189ff581dc",
+    "url": "/expenseTracker-React/static/js/runtime-main.15ffe7f9.js"
+  }
+]);
